622146dbf11c8a955e9fbef3bed576ea4aaacbd5
Andrew Nguyen <ahn018@ucsd.edu>
2019-01-24 13:03:15 -0800

    All lab 3 files pushed

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,43 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Javascript has taken control");
+		$("#testjs").text("Please wait...");
+		$(".jumbotron p").addClass("active");
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$(".project").click(projectClick);
+}
+
+function projectClick(e)
+{
+	e.preventDefault();
+	console.log("Project clicked");
+	$("#project").click(function(e){
+		$('.project h4').text("Project clicked");
+	});
+    //$(this).css("background-color", "#7fff00");
+	var containingProject = $(this).closest(".project");
+	// containingProject.append("<div class='project-description'>\
+	// <p>Description of the project.</p></div>");
+	// // var containingProject = $(this).closest(".project");
+    // // var description = $(containingProject).find(".project-description");
+    // // if (description.length == 0) {
+    // //    $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");//}
+    // // } else {
+	// //    description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+	// //    	$("#project1").fadeIn();
+	// // }
+
+	var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       //description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+		$(".project-description").hide("<div class='project-description'><p>Description of the project.</p></div>");
+	}
+
 }
